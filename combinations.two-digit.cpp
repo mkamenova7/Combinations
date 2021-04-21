6ef0c5b1b304306b55b731d717add00810b3d3d1
mkamenova7 <82510207+mkamenova7@users.noreply.github.com>
2021-04-21 13:52:55 +0300

    Add files via upload

---
+++ combinations.two-digit.cpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d;
+    cout<<"Type 4 numbers - a,b,c and d and if their total is a two-digit number, i'll give you all the possible combinations";
+    cin>>a>>b>>c>>d;
+        if(a+b+c+d>9){
+        cout<<a<<b<<c<<d<<endl;
+        cout<<a<<b<<d<<c<<endl;
+        cout<<a<<c<<b<<d<<endl;
+        cout<<a<<c<<d<<b<<endl;
+        cout<<a<<d<<b<<c<<endl;
+        cout<<a<<d<<c<<b<<endl;
+        cout<<b<<a<<c<<d<<endl;
+        cout<<b<<a<<d<<c<<endl;
+        cout<<b<<d<<a<<c<<endl;
+        cout<<b<<d<<c<<a<<endl;
+        cout<<b<<c<<d<<a<<endl;
+        cout<<b<<c<<a<<d<<endl;
+        cout<<c<<a<<b<<d<<endl;
+        cout<<c<<a<<d<<b<<endl;
+        cout<<c<<b<<a<<d<<endl;
+        cout<<c<<b<<d<<a<<endl;
+        cout<<c<<d<<a<<b<<endl;
+        cout<<c<<d<<b<<a;
+        }else{
+            cout<<"Their total is not a two-digit number";
+        }
+    return 0;
+}
